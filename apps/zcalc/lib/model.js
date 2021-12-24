cac8b2391ef35d684c650923577e823983a2653d
akuma-code <akumapl86@gmail.com>
2021-12-24 22:49:05 +0300

    refactor eblet

--- apps/zcalc/lib/model.js
+++ apps/zcalc/lib/model.js
@@ -1,7 +1,7 @@
 const model = {
     calc: `<input type="text" placeholder="ширина" class="rs_calc_w rs_size">
         <input type="text" placeholder="высота" class="rs_calc_h rs_size">
-        <div class="rs_calc_btn"><button onclick="calc_rs()">$$$</button></div>
+        <div class="rs_calc_btn"><button onclick="calc_rs()">Узнать цену</button></div>
         <div class="rs_calc_res" id="calc_res"></div>`,
 
     props(details = []) {
